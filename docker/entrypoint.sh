d17794c22e67e2a1366532532db423c6dd55a23f
mth_sousa <matheus.soares@jacto.com.br>
2025-06-13 15:52:09 -0300

    Automatic docker conversion

---
+++ docker/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /opt/ros/noetic/setup.bash
+source /root/tools_ws/install/setup.bash
+rosrun magical_ros2_conversion_tool ros2_conversion
--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,4 @@
+
 docker run -it --rm --network=host \
-    -v $(pwd):/root/tools_ws/src/ \
+    -v $1:/root/ros1_ws/src/ \
     --name=magical_ros2_conversion_tool magical_ros2_conversion_tool:latest 
